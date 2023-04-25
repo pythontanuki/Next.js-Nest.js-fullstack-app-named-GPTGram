9e979f773e8810a1c3ce792f30597a7eb9e15338
python_tanuki <araki130710@gmail.com>
2023-04-25 16:42:17 +0900

    can see post

---
+++ src/components/Post.tsx
@@ -0,0 +1,91 @@
+import { Avatar, Flex, Text, VStack, Image, Box } from "@chakra-ui/react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { HiOutlineChatBubbleLeft } from "react-icons/hi2";
+import { FiSend } from "react-icons/fi";
+import { RiBookmarkLine } from "react-icons/ri";
+//add verified mark
+// import { MdVerified } from "react-icons/md";
+
+type Props = {
+  avatarImage: {
+    id: number;
+    url: string;
+  };
+  displayName: string;
+  postImage: {
+    id: number;
+    url: string;
+  };
+  commentByPostOwner: string;
+  representativeUserNameOfLikes: string;
+  representativeAvatarOfLikes: {
+    id: number;
+    url: string;
+  };
+  representativeUserNameOfComments_first: string;
+  representativeCommentContent_first: string;
+  representativeUserNameOfComments_second: string;
+  representativeCommentContent_second: string;
+  // verified: boolean;
+  commentAmount: string;
+  postedAt: string;
+};
+
+export const Post: React.FC<Props> = ({
+  avatarImage,
+  displayName,
+  postImage,
+  commentByPostOwner,
+  representativeUserNameOfLikes,
+  representativeAvatarOfLikes,
+  representativeUserNameOfComments_first,
+  representativeCommentContent_first,
+  representativeUserNameOfComments_second,
+  representativeCommentContent_second,
+  commentAmount,
+  postedAt,
+}) => {
+  return (
+    <VStack>
+      <Flex>
+        <Avatar
+          name={`${displayName}+'s Post`}
+          src={avatarImage.url}
+          w={"30px"}
+          h={"30px"}
+        />
+        <Text>{displayName}</Text>
+        {}
+        <Text>{postedAt}</Text>
+      </Flex>
+      <Image
+        src={postImage.url}
+        alt="Any is OK."
+        boxSize={"100px"}
+        objectFit="cover"
+      />
+      <Flex>
+        <AiOutlineHeart />
+        <HiOutlineChatBubbleLeft />
+        <FiSend />
+        <RiBookmarkLine />
+      </Flex>
+      <Flex>
+        <Image
+          src={representativeAvatarOfLikes.url}
+          alt="Any is OK"
+          h={"8px"}
+          w={"8px"}
+        />
+        <Text>{`${representativeUserNameOfLikes}、その他が「いいね！」しました`}</Text>
+      </Flex>
+      <Text>{`${displayName} ${commentByPostOwner}`}</Text>
+      <Text>{`コメント${commentAmount}件をすべて見る`}</Text>
+      <Text>
+        {`${representativeUserNameOfComments_first} ${representativeCommentContent_first}`}
+        {`${representativeUserNameOfComments_second} ${representativeCommentContent_second}`}
+      </Text>
+      <Text>コメントを追加</Text>
+    </VStack>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import SideBar from "@/components/SideBar";
-import { Flex, HStack, Avatar, Box } from "@chakra-ui/react";
+import { Flex, HStack, Avatar, Box, VStack } from "@chakra-ui/react";
 import { IoIosArrowForward } from "react-icons/io";
+import { Post } from "@/components/Post";
 
 const Home = () => {
   return (
@@ -8,75 +9,96 @@ const Home = () => {
       <Flex>
         <SideBar />
         <IoIosArrowForward />
-        <HStack
-          ml={"150px"}
-          alignItems={"flex-start"}
-          border={"1px"}
-          borderColor={"gray.300"}
-          rounded={"xl"}
-          h={"120px"}
-          w={"470px"}
-          spacing={"0px"}
-          gap={"15px"}
-          boxShadow={"base"}
-          py={"28px"}
-          mt={"16px"}
-          overflow={"scroll"}
-          sx={{
-            "::-webkit-scrollbar": {
-              display: "none",
-            },
-          }}
-        >
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-          <Avatar
-            name="Kent Dodds"
-            src="https://bit.ly/kent-c-dodds"
-            w={"65px"}
-            h={"65px"}
-          />
-        </HStack>
+        <VStack>
+          <HStack
+            ml={"150px"}
+            alignItems={"flex-start"}
+            border={"1px"}
+            borderColor={"gray.300"}
+            rounded={"xl"}
+            h={"120px"}
+            w={"470px"}
+            spacing={"0px"}
+            gap={"15px"}
+            boxShadow={"base"}
+            py={"28px"}
+            mt={"16px"}
+            overflow={"scroll"}
+            sx={{
+              "::-webkit-scrollbar": {
+                display: "none",
+              },
+            }}
+          >
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+            <Avatar
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+              w={"65px"}
+              h={"65px"}
+            />
+          </HStack>
+          <Box>
+            <Post
+              avatarImage={{ id: 1, url: "" }}
+              displayName="taichi"
+              postImage={{ id: 1, url: "messi_kissShoes.jpeg" }}
+              commentByPostOwner="Imada is Superb."
+              representativeUserNameOfLikes="minagawa"
+              representativeAvatarOfLikes={{
+                id: 1,
+                url: "messi_kissShoes.jpeg",
+              }}
+              representativeUserNameOfComments_first="hamada"
+              representativeCommentContent_first="so good!!"
+              representativeUserNameOfComments_second="watanabe"
+              representativeCommentContent_second="so bad!!"
+              commentAmount="10万"
+              postedAt="ちょっと前"
+            />
+          </Box>
+        </VStack>
       </Flex>
     </>
   );
